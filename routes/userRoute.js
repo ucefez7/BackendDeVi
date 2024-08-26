137616948405170ec00bd72986d5b979342298a4
ucefez7 <yousafalieamonu@gmail.com>
2024-08-26 14:52:07 +0530

    Create user or  Login removed the authMiddleware

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -11,11 +11,15 @@ const userAuthMiddleware = require('../middlewares/userAuthMiddleware');
 
 const router = express.Router();
 
+
+//router.post('/', userAuthMiddleware, createOrLoginUser);
+router.post('/', createOrLoginUser);
 // router.get('/', userAuthMiddleware, getUsers);
 router.get('/', getUsers);
 // router.get('/:id', userAuthMiddleware, getUserById);
 router.get('/:id', getUserById);
-router.get('/search', userAuthMiddleware, searchUsersByName);
+// router.get('/search', userAuthMiddleware, searchUsersByName);
+router.get('/search',searchUsersByName);
 router.post('/', userAuthMiddleware, createOrLoginUser);
 router.put('/:id', userAuthMiddleware, updateUser);
 router.delete('/:id', userAuthMiddleware, deleteUser);
