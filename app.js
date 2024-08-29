ca8ff3dad7b23c488b26b663b670bd8f72535ffd
ucefez7 <yousafalieamonu@gmail.com>
2024-08-29 15:56:24 +0530

    test for POST call by res msg

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use(morgan('dev')); // Use 'dev' format for concise colored output
+app.use(morgan('dev')); 
 
 // Routes
 app.use('/api/auth', authRoutes);
