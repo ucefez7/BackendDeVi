133dec3139ab02a720949f2ce956949cd9c51f35
ucefez7 <yousafalieamonu@gmail.com>
2024-09-03 14:03:06 +0530

    post management commit before getting saved post

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -5,8 +5,6 @@ const userAuthMiddleware = require('../middlewares/userAuthMiddleware');
 
 // Post-related routes
 router.post('/posts/create-post', userAuthMiddleware, postController.createPost);
-// router.get('/posts/all', userAuthMiddleware, postController.getAllPosts);
-// router.get('/posts/:postId', userAuthMiddleware, postController.getPostById);
 router.get('/posts/all', postController.getAllPosts);
 router.get('/posts/:postId', postController.getPostById);
 router.put('/posts/:postId', userAuthMiddleware, postController.updatePost);
@@ -17,5 +15,14 @@ router.post('/posts/comment', userAuthMiddleware, postController.addComment);
 router.delete('/posts/comment', userAuthMiddleware, postController.deleteComment);
 router.post('/posts/like/:postId', userAuthMiddleware, postController.likePost);
 router.post('/posts/unlike/:postId', userAuthMiddleware, postController.unlikePost);
+
+
+// Save/Remove Saved Post routes
+router.post('/posts/save/:postId', userAuthMiddleware, postController.savePost);
+router.delete('/posts/save/:postId', userAuthMiddleware, postController.removeSavedPost);
+router.get('/posts/saved', ()=>{
+    console.log("evde ok ann, data verind");
+});
+// router.get('/posts/saved', userAuthMiddleware, postController.getSavedPosts);
 
 module.exports = router;
