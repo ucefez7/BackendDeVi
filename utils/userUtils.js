c97640ccd9b7775fed451f97077ad79b6320a7e0
ucefez7 <yousafalieamonu@gmail.com>
2024-09-17 19:58:10 +0530

    life span of jwt extended to 90 days

--- utils/userUtils.js
+++ utils/userUtils.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 require ('dotenv').config();
 
 const signToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '90d' });
+  //return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
 };
 
 const verifyToken = (token) => {
